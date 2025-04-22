d8da2f4c1164e25a27ddf9d2d039e55d573e1e8d
juan montero <monterotjuan4@gmail.com>
2025-04-22 10:10:19 -0600

    subida de archivos 2

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
-$servidor = "localhost";  // Cambia esto si tu servidor no está en localhost
+$servidor = "base";  // Cambia esto si tu servidor no está en localhost
 $usuario = "root";        // Cambia esto si tu usuario de MySQL no es 'root'
-$contraseña = "";         // Cambia esto si tienes contraseña
+$contraseña = "1234";         // Cambia esto si tienes contraseña
 $base_de_datos = "taqueria";
 
 $conexion = new mysqli($servidor, $usuario, $contraseña, $base_de_datos);
